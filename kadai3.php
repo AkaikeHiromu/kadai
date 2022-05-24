ec44c2593df086da51487b80d8df28ea5689903c
Hiromu <h.spomga.0718eto@gmail.com>
2022-05-24 17:19:57 +0900

    5月24日進捗

--- kadai3.php
+++ kadai3.php
@@ -6,6 +6,8 @@ class Person {
 	public $birthday = '';
 	// 性別(m:男性, f:女性)
 	public $gender = '';
+	//年齢
+	public $age = '';
 
   //各情報を初期化している
 	public function __construct($name,$birthday,$gender){
@@ -28,19 +30,38 @@ class Person {
 
 	//現在の年齢を出すメソッド
 	public function getAge($birthday){
-		$now = new DateTime();
-		echo $now->format('Ymd');
-		//文字列型を整数型に変換
-		$intnow = $now->format('Ymd');
-		(int)$intnow;
+		$today = date('Ymd');
+		$this->age = floor(((int)$today - (int)$birthday) / 10000) . '歳';
+		return $this->age;
 	}
 }
 
-$mypro = new person("ひろむ","20020718","m");
+//Personを継承して出身地を趣味ののプロパティをコンストラクタで初期化する
+class Profile extends Person{
+	//出身地のプロパティを定義
+	public $hometown = '';
+	//趣味のプロパティを定義
+	public $hobby ='';
+
+	//各情報を初期化する
+	public function __construct($hometown,$hobby){
+		$this->hometown = $hometown;
+		$this->hobby = $hobby;
+	}
+	public function selfIntroduction(){
+		return '私の名前は'.$this->name.'です。'.$this->age.'才'.$this->gender.'です。'."\n".'出身は'.$this->hometown.'趣味は'.$this->hobby.'です。';
+		}
+	}
+
+$mypro = new Profile("ひろむ","20020718","m","静岡","旅");
+//名前出力
 echo $mypro->name;
+//生年月日出力
 echo $mypro->birthday;
+//性別出力
 echo $mypro->gender;
+//自己紹介文作成
 echo $mypro->selfIntroduction();
-echo $mypro->getAge($mypro -> birthday);
+echo $mypro->getAge($mypro -> birthday)."\n";//()の中身は引数としてmyproの中のbirthdayを渡している。
 
 ?>
