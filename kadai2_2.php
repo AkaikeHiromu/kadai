a38c290e43cf19f4a903856c2a8c9411ed4205f3
Hiromu <h.spomga.0718eto@gmail.com>
2022-06-17 16:20:29 +0900

    Create kadai2_2.php

---
+++ kadai2_2.php
@@ -0,0 +1,5 @@
+<?php
+$str = file_get_contents('https://map.yahooapis.jp/search/zip/V1/zipCodeSearch?query=105-0011&appid=dj00aiZpPTdMM2piRTFVUldFSyZzPWNvbnN1bWVyc2VjcmV0Jng9ODE-');
+
+echo $str;
+ ?>
